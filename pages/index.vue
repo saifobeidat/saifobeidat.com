e9d8d33be23487b5b3cbd25d1e511fcb10ca7638
Saif <saifobeidat@gmail.com>
2022-04-30 17:20:41 +0300

    init

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="h-screen flex items-center justify-center">
+    This is Saif Obeidat!
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
