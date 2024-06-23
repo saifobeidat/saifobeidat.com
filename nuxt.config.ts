fb594369e4224329b4654239bb30e1cd73cc8884
Saif Obeidat <saifobeidat@gmail.com>
2024-06-23 15:14:15 +0300

    upgrade

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,7 +15,14 @@ export default defineNuxtConfig({
   content: {
     highlight: {
       // Theme used in all color schemes.
-      theme: "github-light"
+      theme: {
+        // Default theme (same as single string)
+        default: "github-light",
+        // Theme used if `html.dark`
+        dark: "github-dark",
+        // Theme used if `html.sepia`
+        sepia: "monokai"
+      }
     }
   }
 });
--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,4 +1,6 @@
 <template>
+  <!-- prose-code:rounded prose-code:font-light prose-code:py-0.5 prose-code:px-2 prose-sm prose-code:bg-slate-100 prose-code:border prose-code:border-slate-300 prose-code:before:content-none prose-code:after:content-none dark:prose-code:bg-slate-600 dark:prose-code:border-slate-500 -->
+
   <article class="">
     <ContentDoc />
   </article>
