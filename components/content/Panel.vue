bd8e287c635200e9cdad20a578f7e164e7b920c6
Saif Obeidat <saifobeidat@gmail.com>
2024-07-01 16:38:52 +0300

    add content list

--- components/content/Panel.vue
+++ components/content/Panel.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="text-black bg-blue-200 rounded-lg border border-blue-300 p-4">
-    <Icon
+    <!-- <Icon
       name="mdi:information-slab-circle-outline"
       class="w-5 mr-1 text-lg shrink-0"
       style="color: black"
-    />
+    /> -->
 
     <ContentSlot :use="$slots.default" unwrap="p" />
   </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,19 @@
 <template>
-  <div></div>
+  <div>
+    <nav>
+      <template>
+        <main>
+          <ContentList path="/blog" v-slot="{ list }">
+            <nav v-for="article in list" :key="article._path">
+              <div>
+                <nuxt-link :to="article._path">{{ article.title }}</nuxt-link>
+              </div>
+            </nav>
+          </ContentList>
+        </main>
+      </template>
+    </nav>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
